a1ba266e3d7d2a938da3d49108eb9245d906ad2c
pratos <pratham.sarang9@gmail.com>
2018-08-16 10:37:55 +0530

    Moving the code

--- germancc-api/src/client.R
+++
@@ -1,3 +0,0 @@
-library(httr)
-
-r <- POST("127.0.0.1:8080/predict", body = toJSON(head(german_credit)), encode = 'json')
--- germancc-api/src/plumber.R
+++
@@ -1,5 +0,0 @@
-setwd("/Users/pratos/difference-engine/docker-for-data-science-r/germancc-api/")
-
-pr <- plumber::plumb("./src/api-function.R")
-pr$run(port = 8080)
-
